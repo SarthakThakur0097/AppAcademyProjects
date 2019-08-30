2f7d4e3fddae51a225ed2b421ab1e448707ce2a6
Sarthak Thakur <sarthakthakur0097@gmail.com>
2019-08-30 16:25:56 -0400

    Added ErrorHandling Project

---
+++ ErrorHandlingExample/ErrorHandlingExample/Default.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ErrorHandlingExample._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:TextBox ID ="Number1" runat ="server"></asp:TextBox>
+    <asp:TextBox ID ="Number2" runat ="server"></asp:TextBox>
+    <asp:Button ID ="Sum" runat ="server" Text ="Add" OnClick ="Sum_Click" />
+    <asp:Label ID ="Result" runat ="server"></asp:Label>
+
+    <asp:HyperLink NavigateUrl ="~/Default.aspx" runat ="server" Text ="Clear"></asp:HyperLink>
+
+
+</asp:Content>
---
+++ ErrorHandlingExample/ErrorHandlingExample/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ErrorHandlingExample
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Sum_Click(object sender, EventArgs e)
+        {
+
+            int result, result2;
+            
+            bool intNum1 = int.TryParse(Number1.Text, out result);
+            bool intNum2 = int.TryParse(Number2.Text, out result2);
+            
+            if(intNum1 && intNum2)
+            {
+                int toAdd = int.Parse(Number1.Text);
+                int toAdd2 = int.Parse(Number2.Text);
+                int sum = (toAdd + toAdd2);
+
+                System.Diagnostics.Debug.Assert(sum != 21);
+                Result.Text = "" + sum;
+                
+                Trace.Write("The sum is: " + sum);
+            }
+            else
+            {
+                Trace.Write("ParseError", "Did not enter valid numbers");
+                Trace.Warn("ParseError", "Did not enter valid numbers");
+
+            }
+
+        }
+    }
+}
