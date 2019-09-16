7dafc3351021817007df2c33aba3c71d8b6b975f
Sarthak Thakur <sarthakthakur0097@gmail.com>
2019-09-16 15:31:52 -0400

    Added Twits

---
+++ Twits/Twits/Models/Twit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Twits.Models
+{
+    public class Twit
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTimeOffset CreatedOn { get; internal set; }
+    }
+}
