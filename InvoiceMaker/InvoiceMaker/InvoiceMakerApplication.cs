36c742ab4502e0f7a41081208e4c0057fdd226a9
Sarthak Thakur <sarthakthakur0097@gmail.com>
2019-09-16 15:37:53 -0400

    InvoiceMaker v1

---
+++ InvoiceMaker/InvoiceMaker/InvoiceMakerApplication.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+using InvoiceMaker.Initialization;
+using System.Web.Routing;
+
+namespace InvoiceMaker
+{
+    public class InvoiceMakerApplication : HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {   
+            RouteConfiguration.AddRoutes(RouteTable.Routes);
+
+            //Debug.WriteLine("Application_Start");
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Application_End");
+        }
+
+        public override void Init()
+        {
+            base.Init();
+
+            BeginRequest += HandleBeginRequest;
+            AuthenticateRequest += HandleAuthenticateRequest;
+        }
+
+        protected void HandleBeginRequest(object sender, EventArgs e)
+        {
+            Debug.WriteLine("HandleBeginRequest");
+        }
+
+        private void HandleAuthenticateRequest(object sender, EventArgs e)
+        {
+            Debug.WriteLine("HandleAuthenticateRequest");
+        }
+
+       
+    }
+}
