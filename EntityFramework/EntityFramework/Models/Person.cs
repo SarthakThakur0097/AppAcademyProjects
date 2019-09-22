b4d3799d512eaf6538876353681f5e95da04976a
Sarthak Thakur <sarthakthakur0097@gmail.com>
2019-09-22 14:55:46 -0400

    InvoiceMakerV2

---
+++ EntityFramework/EntityFramework/Models/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Models
+{
+    [Table("FunnyPeople")]
+    public class Person
+    {
+        public int Id { get; set;}
+        [Column("FullName")]
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool IsStarWarsFan { get; set; }
+
+        public decimal Weight { get; set; }
+
+        //This property doesn't have a setter
+        //So Ef will ignore it when generating the database;
+        public string Info
+        {
+            get
+            {
+                return $"{Name} ({BirthDate})";
+
+            }
+        }
+    }
+}
